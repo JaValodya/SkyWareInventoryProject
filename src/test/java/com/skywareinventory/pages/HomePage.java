06b163d5c633bf566bb572473f6cabe11aa8ecab
[Volodymyr Bondarenko] <[RedefinePL@gmail.com]>
2018-07-30 19:47:13 -0400

    kamil branch second attemt to merge the file

--- src/test/java/com/skywareinventory/pages/HomePage.java
+++ src/test/java/com/skywareinventory/pages/HomePage.java
@@ -1,21 +1,22 @@
 package com.skywareinventory.pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import com.skywareinventory.utilities.Driver;
+import com.google.common.graph.NetworkBuilder;
 
 public class HomePage {
-	public HomePage() {
-		PageFactory.initElements(Driver.getDriver(), this);
+
+	
+
+	public HomePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
 	}
 
-	/*
-	 * Verify the following: 1)www.skywareinventory.com home page appears. 2)Title
-	 * of a loaded page is equal to expected title
-	 */
-	@FindBy(id = "logo")
-	public WebElement title;
+	@FindBy(xpath = "//a[@class ='button login']")
+	public WebElement loginButton;
 
+	
 }
