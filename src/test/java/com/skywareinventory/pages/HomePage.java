9f5953c2b04f161e53a3122150b3fcccab03fdbb
[Volodymyr Bondarenko] <[RedefinePL@gmail.com]>
2018-07-30 20:29:14 -0400

    kamil brnch 4 merge attemt

--- src/test/java/com/skywareinventory/pages/HomePage.java
+++ src/test/java/com/skywareinventory/pages/HomePage.java
@@ -1,23 +1,22 @@
 package com.skywareinventory.pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import com.skywareinventory.utilities.Driver;
+import com.google.common.graph.NetworkBuilder;
 
 public class HomePage {
-	public HomePage() {
-		PageFactory.initElements(Driver.getDriver(), this);
-	}
 
-	/*
-	 * Verify the following: 1)www.skywareinventory.com home page appears. 2)Title
-	 * of a loaded page is equal to expected title
-	 * crasi line
-	 */
-	@FindBy(id = "logo")
-	public WebElement title;
 	
 
+	public HomePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//a[@class ='button login']")
+	public WebElement loginButton;
+
+	
 }
