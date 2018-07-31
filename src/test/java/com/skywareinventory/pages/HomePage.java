0f6a31c27414a2b9a893cf0da3141a0fe207e508
[Volodymyr Bondarenko] <[RedefinePL@gmail.com]>
2018-07-31 13:11:29 -0400

    two verthions of HomePage copyPaste merge

--- src/test/java/com/skywareinventory/pages/HomePage.java
+++ src/test/java/com/skywareinventory/pages/HomePage.java
@@ -1,5 +1,6 @@
 package com.skywareinventory.pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -7,6 +8,8 @@ import org.openqa.selenium.support.PageFactory;
 import com.skywareinventory.utilities.Driver;
 
 public class HomePage {
+	
+	//delbar
 	public HomePage() {
 		PageFactory.initElements(Driver.getDriver(), this);
 	}
@@ -17,5 +20,13 @@ public class HomePage {
 	 */
 	@FindBy(id = "logo")
 	public WebElement title;
+	
+	//kamil 
+	public HomePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//a[@class ='button login']")
+	public WebElement loginButton;
 
 }
